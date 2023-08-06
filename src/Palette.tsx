0a8aa2353fa1ecb6e4ee85815d6b4556b0dad252
James Mortemore <jamesmortemore@gmail.com>
2023-08-06 21:38:39 +0100

    fix: build

--- src/Palette.tsx
+++ src/Palette.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React, { Fragment, ReactNode } from "react";
 import { PaletteState, usePalette } from "./usePalette";
 
 export type PaletteProps = {
@@ -12,5 +12,5 @@ export const Palette: React.FC<PaletteProps> = ({
 }: PaletteProps) => {
   const palette = usePalette(src);
 
-  return <>{children(palette)}</>;
+  return <Fragment>{children(palette)}</Fragment>;
 };
